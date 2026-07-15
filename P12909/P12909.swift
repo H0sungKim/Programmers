44e6e0bb407bb43df1b22a915c85dc667db86fd3
Hosung Kim <hosungkim5108@gmail.com>
2026-07-15 15:33:23 +0900

    Add P12909

---
+++ P12909/P12909.swift
@@ -0,0 +1,47 @@
+//
+//  P12909.swift
+//  P12909
+//
+//  Created by Hosung.Kim on 2026.07.15.
+//
+
+import Testing
+
+struct P12909 {
+    
+    func solution(_ s: String) -> Bool {
+        var parenthesesCount: Int = 0
+        
+        for char in s {
+            switch char {
+            case "(":
+                parenthesesCount += 1
+            case ")":
+                guard parenthesesCount > 0 else { return false }
+                parenthesesCount -= 1
+            default:
+                return false
+            }
+        }
+        
+        guard parenthesesCount == 0 else { return false }
+        return true
+    }
+    
+    @Test func testCase() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+        
+        let testCases: [(input: String, expected: Bool)] = [
+            ("()()", true),
+            ("(())()", true),
+            (")()(", false),
+            ("(()(", false),
+        ]
+        for testCase in testCases {
+            #expect(solution(testCase.input) == testCase.expected)
+        }
+    }
+
+}
